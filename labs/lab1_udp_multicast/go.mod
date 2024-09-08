3ee1192c1e82ee7bdefbf3c0f052482950ad9698
Daniil Solovei <gotodeeps@gmail.com>
2024-09-08 16:29:55 +0700

    feat: add code udp_multicast

---
+++ labs/lab1_udp_multicast/go.mod
@@ -0,0 +1,5 @@
+module lab1_udp_multicast
+
+go 1.22
+
+require github.com/google/uuid v1.6.0
---
+++ labs/lab1_udp_multicast/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	port           = ":9999"         // Порт для обмена сообщениями
+	interval       = 2 * time.Second // Интервал между отправкой сообщений
+	expiryDuration = 5 * time.Second // Время, через которое копия считается исчезнувшей
+)
+
+var (
+	peers     = make(map[string]time.Time) // Список IP-адресов живых копий
+	peersLock sync.Mutex                   // Для синхронизации доступа к списку
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: go run main.go <multicast-address>")
+	}
+
+	multicastAddress := os.Args[1]
+
+	go receive(multicastAddress)
+	go send(multicastAddress)
+
+	// Обновление и вывод списка живых копий
+	for {
+		time.Sleep(1 * time.Second)
+		printAlivePeers()
+	}
+}
+
+// Отправка сообщения о присутствии
+func send(multicastAddress string) {
+	addr, err := net.ResolveUDPAddr("udp", multicastAddress+port)
+	if err != nil {
+		log.Fatalf("Ошибка разрешения адреса: %v", err)
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatalf("Ошибка при подключении к UDP: %v", err)
+	}
+	defer conn.Close()
+
+	for {
+		_, err := conn.Write([]byte("I'm here"))
+		if err != nil {
+			log.Printf("Ошибка отправки сообщения: %v", err)
+		}
+		time.Sleep(interval)
+	}
+}
+
+// Прием сообщений от других копий
+func receive(multicastAddress string) {
+	addr, err := net.ResolveUDPAddr("udp", multicastAddress+port)
+	if err != nil {
+		log.Fatalf("Ошибка разрешения адреса: %v", err)
+	}
+
+	conn, err := net.ListenMulticastUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatalf("Ошибка при подключении к multicast: %v", err)
+	}
+	defer conn.Close()
+
+	conn.SetReadBuffer(1024)
+
+	buffer := make([]byte, 1024)
+	for {
+		n, src, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			log.Printf("Ошибка при получении сообщения: %v", err)
+			continue
+		}
+
+		message := strings.TrimSpace(string(buffer[:n]))
+		if message == "I'm here" {
+			updatePeer(src.IP.String())
+		}
+	}
+}
+
+// Обновление времени активности копии
+func updatePeer(ip string) {
+	peersLock.Lock()
+	defer peersLock.Unlock()
+
+	peers[ip] = time.Now()
+}
+
+// Печать списка живых копий
+func printAlivePeers() {
+	peersLock.Lock()
+	defer peersLock.Unlock()
+
+	now := time.Now()
+	changed := false
+
+	// Удаление неактивных копий
+	for ip, lastSeen := range peers {
+		if now.Sub(lastSeen) > expiryDuration {
+			delete(peers, ip)
+			changed = true
+		}
+	}
+
+	if changed {
+		fmt.Println("Текущие живые копии:")
+		for ip := range peers {
+			fmt.Printf("- %s\n", ip)
+		}
+		fmt.Println("---------------------------")
+	}
+}
